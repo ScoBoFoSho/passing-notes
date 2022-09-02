ba8618c947f108d03b2342822822ecec8982c9c8
Cisco Ferre <ferrecisco@icloud.com>
2022-09-02 16:08:05 -0700

    PORT Update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3333;
 const { notes } = require('./develop/db/db.json');
 const app = express();
 
